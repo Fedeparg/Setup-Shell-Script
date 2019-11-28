9b2a7b01fc67e1907300c8bce0fef05fdb877b7a
Federico Pardo <fpg23feb98@gmail.com>
2019-11-28 12:56:42 +0100

    Updated to use stable Java release

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ sudo apt upgrade -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
-#Add Java 12 Repository
-sudo add-apt-repository ppa:linuxuprising/java
-
 #Install all programs
 sudo apt install google-chrome-stable -y
 sudo apt install python3-pip -y
@@ -21,14 +18,7 @@ sudo snap install code --classic
 sudo apt install texlive -y
 sudo apt install texlive-lang-spanish -y
 sudo apt install texstudio -y
-sudo apt install oracle-java12-installer -y
-
-#Setup Java enviornment
-sudo apt install oracle-java12-set-default
-sudo cat >> /etc/environment <<EOL
-JAVA_HOME=/usr/lib/jvm/java-12-oracle
-JRE_HOME=/usr/lib/jvm/java-12-oracle/jre
-EOL
+sudo apt install default-jdk
 
 #Cleaning files
 sudo apt-get autoclean
@@ -37,10 +27,11 @@ sudo apt autoremove
 
 #Git configuration
 git config --global core.editor "code -w"
-git config --global user.name "Your name here"
-git config --global user.email "youremail@mail.com"
+git config --global user.name "Your name"
+git config --global user.email "Youremail@mail.com"
 
 #Install zsh
 sudo apt install zsh -y
 #Now install "oh-my-zsh" for default configuration
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
